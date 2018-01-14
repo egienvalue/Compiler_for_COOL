cdd5c545e38a1353d2e266d1368bf78fc18b1323
egienvalue <juluo@umich.edu>
2018-01-14 17:56:38 -0500

    ruby version of topological sort

---
+++ pa1/rosetta.rb
@@ -0,0 +1,46 @@
+# RUBY: Reverse-sort the lines from standard input
+require 'set'
+lines = [ ]     # a list variable to hold all the lines we'll read in
+working = true  # are there still more lines to read in? 
+while working
+  line = gets           # read a line from standard input 
+  if line == nil        # nil is "nothing, it didn't work" 
+    working = false     # we're done reading stuff
+  else
+    lines[lines.length] = line # append 'line' to the end of 'lines
+  end # end of 'if'
+end # end of 'while' 
+$t_visited = [ ]
+$p_visited = [ ]
+vertices = lines.to_set
+vertices = vertices.sort do |a,b| a<=>b end
+edge = Struct.new(:left, :right)
+edges = []
+i=0
+until i > lines.length-2 do
+	temp = edge.new(lines.at(i),lines.at(i+1))
+	i = i+2
+	edges.push temp
+end
+edges = edges.sort do |a,b| a.right <=> b.right end
+
+def dfs(current,edges)
+	if $p_visited.include?(current)
+		return 0;
+	end	
+	if $t_visited.include?(current)
+		puts "cycle\n"
+		exit
+	end
+	$t_visited.push current
+	edges.each do |one_edge|
+		if one_edge.left == current
+			dfs(one_edge.right,edges)
+		end
+	end
+	$p_visited.push current
+	return 0;
+end
+vertices.each do |one_vertice| dfs(one_vertice,edges) end
+$p_visited.each do |x| puts x end
+
