baaf6867eab02a94ebad38712930d4678f3827d0
egienvalue <juluo@umich.edu>
2018-03-17 14:49:08 -0400

    add support for Let

--- pa5/main.py
+++ pa5/main.py
@@ -10,6 +10,8 @@ self_reg_d = R(12,"d")
 acc_reg_d = R(13, "d")
 temp_reg_d = R(14, "d")
 
+tab_6 = "{:<24}".format("")
+
 rax = RAX()
 eax = EAX()
 rbx = RBX() 
@@ -30,9 +32,11 @@ int_context_offset = 24
 
 # use these variables to keep tracking info
 label = 0
+#string_map = {"the.empty.string":"", "percent.d":"%ld", "percent.ld":" %ld"}
 string_map = {}
+symbol_table = {}
 
-def attr2asm(attributes):
+def attr2asm(cls_name, attributes):
     global class_map
     global imp_map
     global parent_map
@@ -40,10 +44,13 @@ def attr2asm(attributes):
 
     # this function is used to print attribute constructor
 
-    tab_6 = "{:<24}".format("")
-
-    ret = tab_6 + "## initialize attributes\n"
+    if attributes != [] :
+        ret = tab_6 + "## initialize attributes\n"
+    else:
+        ret = ""
     for i, attr in enumerate(attributes):
+        # insert into symbol_table
+        symbol_table[attr.attr_name.ident] = [str(MEM(24 + 8*i, self_reg))]
         ret += tab_6 +  "## self[%d] holds field %s (%s)\n" % (i+3,
                 attr.attr_name.ident, attr.attr_type.ident)
         if attr.attr_type.ident != "SELF_TYPE":
@@ -64,15 +71,24 @@ def attr2asm(attributes):
             ret+= tab_6 + "## self[%d] %s initializer <- %s\n" % \
                 (i+3,attr.attr_name.ident,str(attr.exp.exp_type))  
             ret += cgen(attr.exp) 
-            ret += str(MOV("q", acc_reg, MEM(24+8*i, self_reg)))
+            ret += str(MOV("q", acc_reg, MEM(24+8*i, self_reg))) + "\n"
         else:
-            ret+= tab_6 + "## self[%d] %s initializer -- none" % \
-                    (i+3, attr.attr_name.ident)
+            ret+= tab_6 + "## self[%d] %s initializer -- none " % \
+                    (i+3, attr.attr_name.ident) + "\n"
     return ret
 
 def cgen(exp):
     ret = ""
     global label
+
+    if isinstance(exp, IdentifierExp):
+        ## b
+        #                movq 32(%r12), %r13
+        variable_name = exp.ident.ident
+        ret += tab_6 + "## %s\n" % variable_name
+        ret += str(MOV("q", symbol_table[variable_name][-1], acc_reg)) + "\n"
+        return ret
+
     if isinstance(exp, New):
         ret += "## new %s" % exp.exp_type + "\n"
         ret += str(PUSH("q", rbp)) + "\n"
@@ -100,8 +116,12 @@ def cgen(exp):
         #                movq %r14, 24(%r13)
         string_key = "string%d" % (len(string_map) + 1)
         string_val = exp.str_val
-        string_map[string_key] = string_val
-        ret += "## %s holds %s" % (string_key, string_val) + "\n"
+        if string_val in string_map.values():
+            string_key = [key for key,val in string_map.iteritems() if val ==
+                            string_val][0] 
+        else:
+            string_map[string_key] = string_val
+        ret += tab_6 + "## %s holds \"%s\"" % (string_key, string_val) + "\n"
         ret += str(MOV("q", "$"+string_key, temp_reg)) + "\n"
         ret += str(MOV("q", temp_reg, MEM(24, acc_reg))) + "\n"
         return ret
@@ -109,7 +129,7 @@ def cgen(exp):
     if isinstance(exp, Integer): 
 
         ret += cgen(New(0,"Int",None))
-        ret += str(MOV("q", exp.int_val, temp_reg)) + "\n"
+        ret += str(MOV("q", "$%d" % exp.int_val, temp_reg)) + "\n"
         ret += str(MOV("q", temp_reg, MEM(int_context_offset, acc_reg))) + "\n"
         return ret
 
@@ -179,7 +199,7 @@ def cgen(exp):
         ret += cgen(exp.rhs)
         # lhs address in acc_reg
         ret += str(MOV("q", MEM(int_context_offset, acc_reg), acc_reg)) + "\n"
-        ret += str(MOV("q", MEM(0, rbp), temp_reg)) + "\n"
+        ret += str(MOV("q", MEM(0, rbp), temp_reg)) + "\n\n"
 
         # Code for Times Operation
         #movq %r14, %rax
@@ -187,7 +207,7 @@ def cgen(exp):
         #shlq $32, %rax
         #shrq $32, %rax
         #movl %eax, %r13d
-        ret += str(MOV("q", acc_reg, MEM(0,rax))) + "\n"
+        ret += str(MOV("q", temp_reg, rax)) + "\n"
         ret += str(IMUL("l", acc_reg_d, eax)) + "\n"
         ret += str(SHL("q", "$32", rax)) + "\n"
         ret += str(SHR("q", "$32", rax)) + "\n"
@@ -240,7 +260,11 @@ def cgen(exp):
         # Creat Space for Exception String
         string_key = "string%d" % (len(string_map) + 1)
         string_val = "ERROR: %s: Exception: division by zero\\n" % exp.line_num
-        string_map[string_key] = string_val
+        if string_val in string_map.values():
+            string_key = [key for key,val in string_map.iteritems() if val ==
+                            string_val][0] 
+        else:
+            string_map[string_key] = string_val
         ret += str(MOV("q", "$"+string_key, acc_reg)) + "\n"
         ret += str(MOV("q", acc_reg, rdi)) + "\n"
         
@@ -250,7 +274,7 @@ def cgen(exp):
         ret += ".globl l%d" % label + "\n"
         ret += "{: <24}".format("l%d:" % label) + "## division is OK\n"
         ret += str(MOV("q", MEM(int_context_offset, acc_reg), acc_reg)) + "\n"
-        ret += str(MOV("q", MEM(0, rbp), temp_reg)) + "\n"
+        ret += str(MOV("q", MEM(0, rbp), temp_reg)) + "\n\n"
         ret += str(MOV("q", "$0", rdx)) + "\n"
         ret += str(MOV("q", temp_reg, rax)) + "\n"
         ret += "cdq\n"
@@ -266,6 +290,20 @@ def cgen(exp):
         ret += str(MOV("q", temp_reg, MEM(int_context_offset, acc_reg))) + "\n"
         return ret
 
+    if isinstance(exp, Let):
+        for idx, binding in enumerate(exp.binding_list):
+            ret += tab_6 + "## fp[%d] holds local %s (%s)\n" % (-idx,
+                    binding.var_ident.ident, binding.type_ident.ident)
+            ret += cgen(binding)
+            # Code for storing the binding back to stack
+            # movq %r13, 0(%rbp)
+            ret += str(MOV("q", acc_reg, MEM(0-8*idx, rbp))) + "\n"
+        ret += cgen(exp.exp)
+        return ret
+
+    if isinstance(exp, Binding):
+        ret += cgen(exp.value_exp)
+        return ret
 def main():
     global class_map
     global imp_map
@@ -341,10 +379,45 @@ def main():
         ret += str(MOV("q", temp_reg, MEM(16,self_reg))) + "\n"
         
         # call function to print attribute
-        ret += attr2asm(attributes)
-        
+        if cls_name not in  ["String", "Int", "Bool"]:
+            ret += attr2asm(cls_name,attributes)
+        else:
+            ### self[3] holds field (raw content) (Int)
+            #            movq $0, %r13
+            #            movq %r13, 24(%r12)
+            #            ## self[3] (raw content) initializer -- none
+            ## Generating Attributes for internal class
+            ret += tab_6 + "## initialize attributes\n"
+            if cls_name in ["Int", "Bool"]:
+                ret += tab_6 + "## self[%d] holds field %s (%s)\n" % (0+3,
+                "(raw content)", "Int")
+                ret += str(MOV("q", "$0", acc_reg)) + "\n"
+            else:
+                ret += tab_6 + "## self[%d] holds field %s (%s)\n" % (0+3,
+                "(raw content)", "String")
+                ret += str(MOV("q", "$the.empty.string", acc_reg)) + "\n"
+            
+            ret += str(MOV("q", acc_reg, MEM(24+8*0, self_reg))) + "\n"
+            ret+= tab_6 + "## self[%d] %s initializer -- none " % \
+                    (0+3, "(raw content)") + "\n"
+
+
+
+        #movq %r12, %r13
+        #                ## return address handling
+        #                movq %rbp, %rsp
+        #                popq %rbp
+        #                ret
+        ret += str(MOV("q", self_reg, acc_reg)) + "\n"
+        ret += tab_6 + "## return address handling\n"
+        ret += str(MOV("q", rbp, rsp)) + "\n"
+        ret += str(POP("q", rbp)) + "\n"
+        ret += tab_6 + "ret\n"
+
         fout.write(ret)
-        exit()
+   
+    #print string_map
+    exit()
         
 
 if __name__ == "__main__":
